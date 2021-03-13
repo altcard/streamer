058eaa894840a3ceec1b7477b5c790b1fa57f55a
Tim K <zacharytcom@outlook.com>
2021-03-13 16:34:57 +0300

    we can now fork, which means we are a daemon now (technically)

--- stream.rb
+++ stream.rb
@@ -7,7 +7,8 @@ def read_settings
 	contents = { "output" => "/tmp/cam.jpeg",
 		     "source" => "/dev/video0",
 		     "socket" => "/tmp/trigger.txt",
-		     "interval" => 0.3 }
+		     "interval" => 0.3,
+		     "fork" => true }
 	confPath = File.join(File.dirname(__FILE__), "config.json")
 
 	if File.exists? confPath then
@@ -45,14 +46,37 @@ def crash_with_socket_deinit(settings)
 	exit 1
 end
 
+def event_loop(settings)
+	while valid_to_continue? settings
+		system("streamer", "-c", settings['source'], "-f", "jpeg", "-o", settings['output']) or crash_with_socket_deinit(settings)
+		sleep settings['interval']
+	end
+end
+
 settings = read_settings
 init_socket(settings)
 
 puts settings
 
-while valid_to_continue? settings
-	system("streamer", "-c", settings['source'], "-f", "jpeg", "-o", settings['output']) or crash_with_socket_deinit(settings)
-	sleep settings['interval']
+if settings.keys.include?("fork") and settings["fork"] then
+	if settings['socket'] == false then
+		puts "Forking without a valid socket file is not supported for obvious reasons I have no need to explain to you"
+		crash_with_socket_deinit(settings)
+	end
+
+	puts "Forking Streamer into a seperate process (delete #{settings['socket']} to stop)"
+	pid = fork do
+		at_exit do
+			FileUtils.rm_f(settings['socket'])
+		end
+
+		event_loop(settings)
+	end
+
+	puts "PID #{pid}"
+else
+	puts "Running in-place"
+	event_loop(settings)
 end
 
 puts "Goodbye"
